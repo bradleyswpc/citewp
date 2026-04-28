40c7e1f53e338a32cac6328272b852191a000cef
Your Name <you@yourdomain.com>
2026-04-28 16:50:28 -0700

    chore: track build/ in git for WP.org distribution

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-plugins', 'wp-primitives'), 'version' => '3cbf3c35c1194b9902db');
